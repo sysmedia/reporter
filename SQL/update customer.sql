21e4f3959a55c0289fcbade7da47952449036142
sysmedia <i2002@126.com>
2019-03-14 14:12:05 +0800

    original commit

---
+++ SQL/update customer.sql
@@ -0,0 +1,13 @@
+update t_shop_customer_count set totalCount = 5312681 where year = 2017 and type = '全部' and shop = 888888;
+update t_shop_customer_count set totalCount = 1648566 where year = 2017 and type = '周末' and shop = 888888;
+update t_shop_customer_count set totalCount = 3664115 where year = 2017 and type = '工作日' and shop = 888888;
+update t_shop_customer_count set totalCount = 3550276 where year = 2018 and type = '全部' and shop = 888888;
+update t_shop_customer_count set totalCount = 1106850 where year = 2018 and type = '周末' and shop = 888888;
+update t_shop_customer_count set totalCount = 2443426 where year = 2018 and type = '工作日' and shop = 888888;
+
+update t_shop_count set avgPrice = 67.31432476 where year = 2017 and type = '全部' and shop = 888888;
+update t_shop_count set avgPrice = 79.71866581 where year = 2017 and type = '周末' and shop = 888888;
+update t_shop_count set avgPrice = 62.21609914 where year = 2017 and type = '工作日' and shop = 888888;
+update t_shop_count set avgPrice = 63.60169644 where year = 2018 and type = '全部' and shop = 888888;
+update t_shop_count set avgPrice = 74.46483281 where year = 2018 and type = '周末' and shop = 888888;
+update t_shop_count set avgPrice = 59.16494733 where year = 2018 and type = '工作日' and shop = 888888;
